18c495422df67f6a0c6d27757b045e37124357c2
ahmedibrahimphd <75679255+ahmedibrahimphd@users.noreply.github.com>
2022-07-12 11:42:33 -0400

    New changes

---
+++ r-script.R
@@ -0,0 +1,17 @@
+install.packages("tidyverse")
+
+library(tidyverse)
+
+sci_data <- read.csv("data/sci-online-classes.csv")
+sci-online-classes
+sci_data
+
+sci_data_pipe <- read_csv("data/sci-online-classes.csv") |> # read in data
+    select(student_id, FinalGradeCEMS, course_id, ) |> # select variables
+    rename(final_grade = FinalGradeCEMS) |> # rename FinalGradeCEMS
+    filter(final_grade > .5) |> # keep grades higher than 50% 
+    drop_na() # remove rows with missing data
+
+add_numbers <- function(number_1, number_2) { number_1 + number_2}
+add_numbers(3, 8)
+View(sci_data)
